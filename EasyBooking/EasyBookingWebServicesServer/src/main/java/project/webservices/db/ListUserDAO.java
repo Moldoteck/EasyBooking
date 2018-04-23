b7116eb0ec9e6b921ef89d85b9f3bddf418594fa
carme_000 <carme_000@Carmen>
2018-04-23 23:07:12 +0300

    Return true Return false

--- EasyBooking/EasyBookingWebServicesServer/src/main/java/project/webservices/db/ListUserDAO.java
+++ EasyBooking/EasyBookingWebServicesServer/src/main/java/project/webservices/db/ListUserDAO.java
@@ -122,9 +122,11 @@ public class ListUserDAO implements UserDAO {
 		}
 		try {
 			statmt.execute("INSERT INTO 'users' ('username', 'password') VALUES ('"+user.getUsername()+"', '"+user.getPassword()+"'); ");
+			return true;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		}
 		//if (user.getUsername() != User.MISSING_ID) {
 		// if the book is already in the db
@@ -138,7 +140,7 @@ public class ListUserDAO implements UserDAO {
             book.setId(nextId++);
         }*/
 		//books.add(book);
-		return true;
+		//return true;
 	}
 
 	/*
