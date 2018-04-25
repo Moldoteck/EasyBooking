31c4c88e0abed96685677b0c945300c637bdd8fc
Cristian Padureac <Moldoteck@users.noreply.github.com>
2018-04-25 20:07:09 +0300

    empalo
    testing

---
+++ EasyBooking/EasyBookingWebServicesServer/src/main/java/project/webservices/service/MyApplication.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Adrian Alexandrescu. All rights reserved.
+ * ADRIAN ALEXANDRESCU PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * See <license.txt> for more details.
+ */
+package project.webservices.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class MyApplication extends Application {
+
+}
